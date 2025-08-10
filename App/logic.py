492ab367124bf79f2770dad249284a880d4dc0ac
friscobarto <f.bartolomeu@uniandes.edu.co>
2025-08-10 18:41:43 -0500

    modificaciones individuales laboratorio 1

--- App/logic.py
+++ App/logic.py
@@ -140,7 +140,14 @@ def load_books_tags(catalog, filename):
     # La función debe recibir el catalogo y el nombre del archivo csv con los tags de los libros.
     # La función debe cargar los tags de los libros del archivo y los agregar al conjunto book_tags del catalogo.
     # La función debe retornar el tamaño del conjunto de tags de los libros.
-    pass
+    books_tags = catalog.get("Books_tags")
+    book_tag_file = os.path.join(data_dir, filename)
+    catalog["Books_tags"] = set.load_set(books_tags, book_tag_file)
+    
+    if books_tags is None:
+        return None
+    else:
+        return set.size(catalog.get("Books_tags"))
 
 # Funciones de consulta
 
--- App/view.py
+++ App/view.py
@@ -60,6 +60,7 @@ def print_menu():
     # TODO: Mods Lab 1, agregar la opcion 3.
     # Agregue la opción 3 para cargar los tags de los libros.
     # Pueede guiarse de las opciones 1 y 2.
+    print("3- Cargar Tags de Libros")
     print("0- Salir")
 
 
@@ -134,8 +135,11 @@ def main():
         # Esta función carga los tags de los libros en el catalogo.
         # Puede guiarse de las opciones 1 y 2.
         # Imprima el total de tags de los libros cargados.
+        
         elif int(inputs[0]) == 3:
-            pass
+            print("Cargando información de tags de libros....")
+            booksTags = load_books_tags(app)
+            print("Total de tags de libros cargados: " + str(booksTags) + "\n")
 
         elif int(inputs[0]) == 0:
             working = False
